dda1c986f69166e99d0949de0ac964c80d07c8f8
MauriceHumado <mauricehumado@gmail.com>
2022-10-13 14:13:24 -0700

    puts(Mystr);

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,2 +1,7 @@
-#!/bin/bash
-puts("Programming is like building a multilingual puzzle\n");
+# include <stdio.h>
+int main ()
+{
+	char Mystr[100] = "Programming is like building a multilingual puzzle";
+	puts(Mystr);
+	return 0;
+}
