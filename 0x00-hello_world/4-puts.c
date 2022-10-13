a4d520e1874210b119d5d87e1061efc910bb73f2
MauriceHumado <mauricehumado@gmail.com>
2022-10-13 16:01:36 -0700

    push

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,8 @@
 #include <stdio.h>
 int main(void)
 {
-	char str[100] = ("\"Programming is like building a multilingual puzzle");
 
-
-	puts(str);
+	puts("\"Programming is like building a multilingual puzzle");
 
 
 	return (0);
