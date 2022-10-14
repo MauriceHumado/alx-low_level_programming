57f05dbf972e089c91ec7b5ed1c7659467930fc8
MauriceHumado <mauricehumado@gmail.com>
2022-10-14 01:46:11 -0700

    write, stderr

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -9,7 +9,10 @@
 
 int main(void)
 {
-	write(STDOUT_FILENO, "and\ that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+	
+	char str1[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+
+	write(2, str1, 59);
 
 	return (1);
 }
