e0baaddcba06a29e8c65efde24e028f231de9eb1
MauriceHumado <mauricehumado@gmail.com>
2022-10-13 22:27:52 -0700

    puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,9 @@
 #include <stdio.h>
 /**
- * main - Entry point 
- * Description: 'the program's description' 
- * Return: Always 0 (Success)      
- */
+ main - Entry point 
+ Description: 'the program's description' 
+ Return: Always 0 (Success)      
+*/
 
 int main(void)
 {
