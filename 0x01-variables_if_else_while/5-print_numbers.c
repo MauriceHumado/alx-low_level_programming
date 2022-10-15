2237859e54bd45112d73279b908ef9fb3d31fa05
MauriceHumado <mauricehumado@gmail.com>
2022-10-15 07:15:06 -0700

    single numbers base 10 from 0

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main-program entry point.
+ * return:0 Always (Success)
+ */
+
+int main(void)
+{
+	char i;
+
+	for (i = '0'; i <= '9'; i++)
+	{
+
+		putchar(i);
+	}
+	putchar('\n');
+	return (0);
+}
