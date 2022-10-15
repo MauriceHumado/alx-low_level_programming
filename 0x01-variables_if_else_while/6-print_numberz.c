495c85814387fbc9368543223f47be44bf2fe77d
MauriceHumado <mauricehumado@gmail.com>
2022-10-15 07:20:45 -0700

    numbers base 10 from 0 using putchar

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main-program entry point.
+ * return:0 Always (Success)
+ */
+
+int main(void)
+{
+	int i;
+
+	for (i = '0'; i <= '9'; i++)
+
+		putchar(i);
+	putchar('\n');
+	return (0);
+}
