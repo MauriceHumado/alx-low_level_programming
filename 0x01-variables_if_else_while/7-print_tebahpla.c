d20d80b4c77e7f2a81a0e33058f6cfb87f31b9ec
MauriceHumado <mauricehumado@gmail.com>
2022-10-16 02:47:58 -0700

    lowercase alphabets in reverse with putchar

--- 0x01-variables_if_else_while/7-print_tebahpla.c
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -1,13 +1,18 @@
 #include <stdio.h>
+
 /**
- * main-program entry point.
- * return:0 Always (Success)
+ * main - Entry point.
+ *
+ * Return: 0 always (Success)
+ *
  */
+
 int main(void)
 {
 	char i;
 
 	for (i = 'z'; i >= 'a'; i--)
+
 		putchar(i);
 	putchar('\n');
 	return (0);
