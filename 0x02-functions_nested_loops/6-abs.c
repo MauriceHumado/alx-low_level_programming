35234c1627e7ad0d21b814cda9608944a2891433
MauriceHumado <mauricehumado@gmail.com>
2022-10-19 08:40:14 -0700

    print absolute value of number

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _abs - absoulture value of an integer
+ * @i: integer
+ * Return: absolute value
+ */
+
+int _abs(int i)
+{
+	if (i >= 0)
+		return (i);
+	else
+		return (-i);
+}
