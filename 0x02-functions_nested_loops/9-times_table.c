0c3422d2ae7e14ece118dca40bf4090696d6d5d4
MauriceHumado <mauricehumado@gmail.com>
2022-10-20 05:12:13 -0700

    print 9 times table

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -21,9 +21,9 @@ void times_table(void)
 			prod = num * mult;
 
 			if (prod <= 9)
-				_pucthar(' ');
+				_putchar(' ');
 			else
-				_putchar((prod / (10) + '0');
+				_putchar((prod / 10) + '0');
 
 			_putchar((prod % 10) + '0');
 		}
