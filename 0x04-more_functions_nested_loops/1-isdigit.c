f66132281e0ba53d2773e217ad06e0656545d332
MauriceHumado <mauricehumado@gmail.com>
2022-10-20 10:10:06 -0700

    check for digit from 0 through 9

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _isdigit - checks for digit zero through nine
+ * Return: 1 if digit otherwise 0
+ * @c: input
+ */
+
+int _isdigit(int c)
+{
+	if (x >= 48 && x <= 58)
+	{
+		return (1);
+	}
+
+	return (0);
+}
