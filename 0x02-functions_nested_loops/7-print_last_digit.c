71de06a4e28f26ff162962805f8414e6e6320c2d
MauriceHumado <mauricehumado@gmail.com>
2022-10-20 02:44:29 -0700

    last digit

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -8,16 +8,14 @@
  * @x: last digit
  */
 
-int print_last_digit(int x)
+int print_last_digit(int n)
 {
 	int x = n % 10;
 
 	if (x < 0)
 		x = -1 * (n % 10);
-	else
-		x = n % 10;
 
 	_putchar(x + '0');
 
-	return (x);
+	return (n % 10);
 }
