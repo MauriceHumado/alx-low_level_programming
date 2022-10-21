ced89692168634b7eb6788a1122e98d2342e9927
MauriceHumado <mauricehumado@gmail.com>
2022-10-21 03:40:49 -0700

    print a square

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -17,9 +17,9 @@ void print_diagonal(int n)
 	}
 	else
 	{
-		for (a = 0; a < n; a++)
+		for (a = 1; a <= n; a++)
 		{
-			for (b = 0; b < a; b++)
+			for (b = 1; b <= a; b++)
 			{
 				_puthcar(' ');
 			}
---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,28 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_square - prints a square
+ * @size: size of square
+ */
+
+void print_square(int size)
+{
+	if (sqr <= 0)
+	{
+		_putchar('\n');
+	}
+	else
+	{
+		int a, b;
+
+		for (a = 0; a < sqr; a++)
+		{
+			for (b = 0; b < sqr; b++)
+			{
+				_putchar('#');
+			}
+			_putchar('n\')
+		}
+	}
+}
