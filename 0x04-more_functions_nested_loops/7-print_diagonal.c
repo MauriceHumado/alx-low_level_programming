ca4b4a88ebbf0cd4c09e8e94e75a5a4e78fbebba
MauriceHumado <mauricehumado@gmail.com>
2022-10-21 03:13:50 -0700

    draw diagonal lines

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -12,14 +12,20 @@ void print_diagonal(int n)
 	int a, b;
 
 	if (n <= 0)
-		_putchar('\n');
-
-	for (a = 0; a < n; a++)
 	{
-		for (b = 0; b < a; b++)
-			_puthcar(' ');
-
-		_putchar('\\');
 		_putchar('\n');
+	}
+	else
+	{
+		for (a = 0; a < n; a++)
+		{
+			for (b = 0; b < a; b++)
+			{
+				_puthcar(' ');
+			}
+
+			_putchar('\\');
+			_putchar('\n');
+		}
 	}
 }
