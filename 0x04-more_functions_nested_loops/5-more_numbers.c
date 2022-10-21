588ab1ed5d539405484d97ce423ca3c52c455d73
MauriceHumado <mauricehumado@gmail.com>
2022-10-21 00:48:36 -0700

    10x numbers from 0 to 14

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -4,21 +4,21 @@
 /**
  * more_numbers - print 10x number from 0 to 14
  * Return: 10times the number
- * @x: one number
- * @y: another number
  */
 
 void more_numbers(void)
 {
+	int y, x;
 
-	for (; y <= 10; y++)
+	for (y = 0; y <= 10; y++)
 	{
 		for (x = 0; x <= 14; x++)
 		{
 			if (x > 9)
 			{
-				_putchar(x % 10 + '0');
+				_putchar(x / 10 + '0');
 			}
+			_putchar(x % 10 + '0');
 		}
 		_putchar('\n');
 	}
