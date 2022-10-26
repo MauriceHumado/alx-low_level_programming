b99631d0b37ca863234ea77a1b101fb8e211f559
MauriceHumado <mauricehumado@gmail.com>
2022-10-25 19:33:35 -0700

    return string

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -3,14 +3,16 @@
 /**
  * _puts - prints a string then a newline to stdout
  * @str: string input
- * Return: nothing
+ * Return: string
  */
 
 void _puts(char *str)
 {
-	for (; *str != '\0'; str++)
+	int i;
+
+	for (i = 0; str[i] != '\0'; i++)
 	{
-		_putchar(*str);
+		_putchar(str[i]);
 	}
 	_putchar('\n');
 }
