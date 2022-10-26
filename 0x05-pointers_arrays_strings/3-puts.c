6b2c392743e87a1a310649c6d43170e7c4983305
MauriceHumado <mauricehumado@gmail.com>
2022-10-25 21:45:36 -0700

    print a string

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -1,5 +1,6 @@
 #include "main.h"
 #include <stdio.h>
+#include <unistd.h>
 
 /**
  * _puts - prints a string then a newline to stdout
--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -11,5 +11,7 @@ void puts2(char *str);
 void puts_half(char *str);
 void print_array(int *a, int n);
 char *_strcpy(char *dest, char *src);
+int _putchar(char c);
+void _putchar(char c);
 
 #endif
