28f0126c5c9baac980a83ad8b64110b0ed517275
MauriceHumado <mauricehumado@gmail.com>
2022-10-27 13:23:15 -0700

    half a string

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -10,18 +10,19 @@
 
 void puts_half(char *str)
 {
-	int i, n, len = 0;
+	int i = 0;
+	int n;
 
-	for (i = 0; str[i] != '\0'; i++)
-		len++;
+	while (str[i] != '\0')
+		i++;
 
-	n = (len / 2);
+	if (i % 2 != 0)
+		n = (i + 1) / 2;
+	else
+		n = i / 2;
 
-	if ((len % 2) == 1)
-		n = ((len - 1) / 2);
-
-	for (i = n; str[i] != '\0'; i++)
-		_putchar(str[i]);
+	for (; str[n] != '\0'; n++)
+		_putchar(str[n]);
 
 	_putchar('\n');
 }
