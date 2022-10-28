6853d772afe6ceabb38267fcb0fd97a26759f687
MauriceHumado <mauricehumado@gmail.com>
2022-10-27 21:57:06 -0700

    copy a string

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -10,13 +10,18 @@
 
 char *_strncpy(char *dest, char *src, int n);
 {
-	int i;
+	int i = 0;
 
-	for (i = 0; src[i] != '\0'; i++)
-		if (i < n)
-			dest[i] = src[i];
-		while (i < n)
-			dest[i++] = '\0';
+	while (src[i] != '\0' && i < n)
+	{
+		*(dest + i) = src[i];
+		i++;
+	}
+	while (i < n)
+	{
+		dest[i] = '\0';
+		i++;
+	}
 
 	return (dest);
 }
