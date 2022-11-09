df4883bcdb53d72e11e89a8a77821c48d9b55f60
MauriceHumado <mauricehumado@gmail.com>
2022-11-09 03:13:46 -0800

    concatenate two strings

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -37,18 +37,13 @@ char *str_concat(char *s1, char *s2)
 		return (NULL);
 
 	for (; i < (lens1 + lens2); i++)
-	{
 		if (i < lens1)
-		{
 			new_str[i] = *s1;
 			s1++;
-		}
 		else
-		{
 			new_str[i] = *s2;
 			s2++;
-		}
-	}
-	new_str[i] = '\0';
+		new_str[i] = '\0';
+
 	return (starts1);
 }
