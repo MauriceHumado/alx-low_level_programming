4eb244d4ba2ff3245c5e2927430d805cabaff5f4
MauriceHumado <mauricehumado@gmail.com>
2022-11-10 10:02:19 -0800

    allocates memory of an array

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -2,7 +2,7 @@
 #include <stdlib.h>
 
 /**
- * _calloc - Allocates memory for an array of a certain number of elements
+ * *_calloc - Allocates memory for an array of a certain number of elements
  * @nmemb: The number of elements
  * @size: The byte size of each array element
  * Return: nmemb = 0, size = 0, or NULL if fail, else pointer to allocated mem
