56acea69d349082b2f5db8efe55a3b602d614d03
MauriceHumado <mauricehumado@gmail.com>
2022-11-10 22:40:16 -0800

    multiplies two positive numbers

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -10,7 +10,7 @@
  * Return: 0 on success;
  */
 
-int main(int argc, char **argv)
+int main(int argc, char *argv[])
 {
 	int num1, num2, result;
 
@@ -23,7 +23,7 @@ int main(int argc, char **argv)
 	{
 		num1 = atoi(argv[1]);
 		num2 = atoi(argv[2]);
-		result = num1 * num2;
+		result = (num1) * (num2);
 		printf("%d\n", result);
 		return (0);
 	}
