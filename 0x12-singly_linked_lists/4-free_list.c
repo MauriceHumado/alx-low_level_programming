39b0f6f5db2d669600e4ec640570a4ee59357175
MauriceHumado <mauricehumado@gmail.com>
2022-11-24 18:16:30 -0800

    frees a list

---
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -0,0 +1,24 @@
+#include "lists.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <stddef.h>
+#include <string.h>
+
+/**
+ * free_list - Frees a list
+ * @head: A pointer to the list head
+ * REturn: nothing
+ */
+
+void free_list(list_t *head)
+{
+	list_t *tmp;
+
+	while (head)
+	{
+		tmp = head->next;
+		free(head->str);
+		free(head);
+		head = tmp;
+	}
+}
