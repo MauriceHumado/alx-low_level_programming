7fba01ea448e623387c140d37c4d64e778899a2c
MauriceHumado <mauricehumado@gmail.com>
2022-12-05 12:11:08 -0800

    include guards

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -9,6 +9,7 @@
 #include <sys/stat.h>
 #include <fcntl.h>
 #include <elf.h>
+#include <sys/uio.h>
 
 int create_file(const char *filename, char *text_content);
 int append_text_to_file(const char *filename, char *text_content);
