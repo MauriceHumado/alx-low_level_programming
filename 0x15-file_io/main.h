a86aea230161dcf9603ee9cbe16418a41f80a608
MauriceHumado <mauricehumado@gmail.com>
2022-12-05 12:08:49 -0800

    add other libraries

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -5,6 +5,11 @@
 #include <unistd.h>
 #include <stdlib.h>
 
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <elf.h>
+
 int create_file(const char *filename, char *text_content);
 int append_text_to_file(const char *filename, char *text_content);
 
