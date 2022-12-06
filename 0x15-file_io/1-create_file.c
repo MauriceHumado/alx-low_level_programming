57c7c9ae6c5b250c517e60bb233bdf63c391cb7f
MauriceHumado <mauricehumado@gmail.com>
2022-12-05 18:41:06 -0800

    create file

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -1,4 +1,6 @@
 #include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
 
 /**
  * create_file - creates a file
@@ -9,21 +11,21 @@
 
 int create_file(const char *filename, char *text_content)
 {
-	int o, w, len = 0;
+	int fd, error, len = 0;
 
 	if (filename == NULL)
 		return (-1);
 
-	if (text_content != NULL)
-	{
-		for (len = 0; text_content[len];)
-			len++;
-	}
+	fd = open(filename, O_RDWR | O_CREAT | O_TRUNC, 0600);
+	if (fd < 0)
+		return (-1);
 
-	o = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
-	w = write(o, text_content, len);
+	while (text_content && *(text_content + len))
+		len++;
 
-	close(o);
+	error = write(fd, text_content, len);
+	close(fd);
 
-	return (1);
+	if (error < 0)
+		return (-1);
 }
