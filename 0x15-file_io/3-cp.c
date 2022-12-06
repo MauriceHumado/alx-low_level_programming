b4f8b88ae88dba5dac565fe333d49ab36a883084
MauriceHumado <mauricehumado@gmail.com>
2022-12-05 22:09:37 -0800

    copy file into file

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -15,37 +15,41 @@
  */
 int main(int ac, char **av)
 {
-	int file_from, file_to;
-	ssize_t  l_read = 1024, l_write, close_file;
-	char content[1024];
+	int input_fd, output_fd, istatus, ostatus;
+	char buf[MAXSIZE];
+	mode_t mode;
 
+	mode = S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP | S_IROTH;
 	if (ac != 3)
-	{ dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
-		exit(97); }
-	file_from = open(av[1], O_RDONLY);
-	if (file_from == -1)
-	{ dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", av[1]),
-		exit(98); }
-	file_to = open(av[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
-	if (file_to == -1)
-	{ dprintf(STDERR_FILENO, "Error: Can't write to %s\n", av[2]);
-		exit(99); }
-	while (l_read == 1024)
-	{
-		l_read = read(file_from, content, 1024);
-		if (l_read == -1)
-			exit(98); }
-	l_write = write(file_to, content, l_read);
-	if (l_write == -1)
-	{ dprintf(STDERR_FILENO, "Error: Can't write to %s\n", av[2]);
-		exit(99); }
-	close_file = close(file_from);
-	if (close_file == -1)
-	{ dprintf(STDERR_FILENO, "Error: Can't close fd  %d\n", file_from);
-		exit(100); }
-	close_file = close(file_to);
-	if (close_file == -1)
-	{ dprintf(STDERR_FILENO, "Error: Can't close fd  %d\n", file_to);
-		exit(100); }
+		dprintf(SE, "Usage: cp file_from file_to\n"), exit(97);
+	input_fd = open(av[1], O_RDONLY);
+	if (input_fd == -1)
+		dprintf(SE, "Error: Can't read from file %s\n", av[1]), exit(98);
+	output_fd = open(av[2], O_CREAT | O_WRONLY | O_TRUNC, mode);
+	if (output_fd == -1)
+		dprintf(SE, "Error: Can't write to %s\n", av[2]), exit(99);
+	do {
+		istatus = read(input_fd, buf, MAXSIZE);
+		if (istatus == -1)
+		{
+			dprintf(SE, "Error: Can't read from file %s\n", av[1]);
+			exit(98);
+		}
+		if (istatus > 0)
+		{
+			ostatus = write(output_fd, buf, (ssize_t) istatus);
+			if (ostatus == -1)
+			{
+				dprintf(SE, "Error: Can't write to %s\n", av[2]);
+				exit(99);
+			}
+		}
+	} while (istatus > 0);
+	istatus = close(input_fd);
+	if (istatus == -1)
+		dprintf(SE, "Error: Can't close fd %d\n", input_fd), exit(100);
+	ostatus = close(output_fd);
+	if (ostatus == -1)
+		dprintf(SE, "Error: Can't close fd %d\n", output_fd), exit(100);
 	return (0);
 }
