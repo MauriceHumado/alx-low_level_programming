0a106745415784aac6e3438e6c127056154aac53
MauriceHumado <mauricehumado@gmail.com>
2022-12-05 20:30:24 -0800

    main

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -14,9 +14,5 @@ ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
 int append_text_to_file(const char *filename, char *text_content);
 int _putchar(char c);
-void write_error(char *write_output_error);
-void read_error(char *read_output_error);
-int safe_close(int description);
-int main(int argc, char *argv[]);
 
 #endif
