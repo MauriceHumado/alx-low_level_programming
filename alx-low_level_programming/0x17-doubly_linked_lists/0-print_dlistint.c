9d5bfcb763b699cb1927499379140c9af9c07630
MauriceHumado <mauricehumado@gmail.com>
2023-01-17 11:06:04 +0000

    print elements

--- alx-low_level_programming/0x17-doubly_linked_lists/0-print_dlistint.c
+++ alx-low_level_programming/0x17-doubly_linked_lists/0-print_dlistint.c
@@ -1,10 +1,10 @@
 #include "lists.h"
 
 /**
- * print_dlistint - prints all the elements of a dlistint_t list.
- * @h: pointer to the list.
- * Return: number of nodes.
-**/
+ * print_dlistint - prints all the elements of a dlistint_t list
+ * @h: pointer to the list
+ * Return: number of nodes
+ **/
 
 size_t print_dlistint(const dlistint_t *h)
 {
